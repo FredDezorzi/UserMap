8ed7ef07ee348eab40ff05bdff0512015dfd5d72
Frederico Dezorzi <fred.dezorzi@gmail.com>
2025-05-10 13:24:51 -0300

    Add Spring Boot project files

---
+++ src/main/java/com/usermap/UsermapApplication.java
@@ -0,0 +1,13 @@
+package com.usermap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsermapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsermapApplication.class, args);
+	}
+
+}

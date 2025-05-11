b323e500ae604caa23be548a6c957c988875598f
Frederico Dezorzi <fred.dezorzi@gmail.com>
2025-05-11 12:12:53 -0300

    Created UserRepository and AddressRepository

---
+++ src/main/java/com/usermap/repository/AddressRepository.java
@@ -0,0 +1,9 @@
+package com.usermap.repository;
+
+import com.usermap.entity.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AddressRepository extends JpaRepository<Address, Long> {
+}
